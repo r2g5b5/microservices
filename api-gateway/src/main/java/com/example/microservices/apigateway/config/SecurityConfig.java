9a28ba4b6432311333018c4c4502907fa63c25f7
Hazhir <programmer4906@gmail.com>
2024-12-16 18:00:41 +0330

    implement key clock

---
+++ api-gateway/src/main/java/com/example/microservices/apigateway/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.microservices.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer.jwt(jwt -> Customizer.withDefaults())
+                ).build();
+    }
+}

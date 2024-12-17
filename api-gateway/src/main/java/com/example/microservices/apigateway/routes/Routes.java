59dffa043c51d7a249fc7a10998221d2fa52b4c7
Hazhir <programmer4906@gmail.com>
2024-12-17 15:11:46 +0330

    implement resilience4j

--- api-gateway/src/main/java/com/example/microservices/apigateway/routes/Routes.java
+++ api-gateway/src/main/java/com/example/microservices/apigateway/routes/Routes.java
@@ -1,17 +1,21 @@
 package com.example.microservices.apigateway.routes;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import java.net.URI;
 import java.util.Map;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 
 @Configuration
 public class Routes {
@@ -34,11 +38,12 @@ public class Routes {
                 "/api/inventory", inventoryServiceUrl
         );
 
-        var gatewayBuilder = GatewayRouterFunctions.route("api_gateway");
+        var gatewayBuilder = route("api_gateway");
 
         routes.forEach((path, url) ->
-                gatewayBuilder.route(RequestPredicates.path(path), HandlerFunctions.http(url)));
-
+                gatewayBuilder.route(RequestPredicates.path(path), HandlerFunctions.http(url))
+                        .filter(CircuitBreakerFilterFunctions.circuitBreaker(url,
+                                URI.create("forward:/fallbackRoute"))));
         return gatewayBuilder.build();
     }
 
@@ -50,13 +55,23 @@ public class Routes {
                 "/aggregate/inventory-service/api-docs", inventoryServiceUrl
         );
 
-        var swaggerBuilder = GatewayRouterFunctions.route("swagger_routes");
+        var swaggerBuilder = route("swagger_routes");
 
         swaggerRoutes.forEach((path, url) ->
                 swaggerBuilder.route(RequestPredicates.path(path), HandlerFunctions.http(url))
+                        .filter(CircuitBreakerFilterFunctions.circuitBreaker(url,
+                                URI.create("forward:/fallbackRoute")))
                         .filter(setPath("/api-docs")));
 
         return swaggerBuilder.build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return route("fallbackRoute")
+                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Service Unavailable, please try again later"))
+                .build();
     }
 
 

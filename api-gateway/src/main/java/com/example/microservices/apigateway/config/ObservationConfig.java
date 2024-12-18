b4d684cd8285f464505a8df2606d102713b0a88e
Hazhir <programmer4906@gmail.com>
2024-12-18 16:54:40 +0330

    fix bugs

--- api-gateway/src/main/java/com/example/microservices/apigateway/config/ObservationConfig.java
+++ api-gateway/src/main/java/com/example/microservices/apigateway/config/ObservationConfig.java
@@ -2,11 +2,23 @@ package com.example.microservices.apigateway.config;
 
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 
 @Configuration
+@RequiredArgsConstructor
 public class ObservationConfig {
+
+    private final ConcurrentKafkaListenerContainerFactory kafkaListenerContainerFactory;
+
+    @PostConstruct
+    public void init() {
+        kafkaListenerContainerFactory.getContainerProperties().setObservationEnabled(true);
+    }
+
     @Bean
     ObservedAspect observedAspect(ObservationRegistry registry) {
         return new ObservedAspect(registry);
--- order-service/src/main/java/com/example/microservices/orderservice/config/RestClientConfig.java
+++ order-service/src/main/java/com/example/microservices/orderservice/config/RestClientConfig.java
@@ -1,8 +1,9 @@
 package com.example.microservices.orderservice.config;
 
 
-
 import com.example.microservices.orderservice.client.InventoryClient;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
@@ -17,16 +18,20 @@ import java.time.Duration;
 
 
 @Configuration
+@RequiredArgsConstructor
 public class RestClientConfig {
 
     @Value("${inventory.url}")
     private String inventoryServiceUrl;
+
+    private final ObservationRegistry observationRegistry;
 
     @Bean
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
                 .requestFactory(getClientRequestFactory())
+                .observationRegistry(observationRegistry)
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
